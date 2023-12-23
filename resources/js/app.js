0073051bd0b127b2372f76029eeef179b63f4798
Mushabberul Islam ( Sabbir Mia ) <88495363+mushabberul@users.noreply.github.com>
2023-12-23 18:22:23 +0600

    mastering and scaffolding

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import './bootstrap';
+
+import '../assets/css/flag-icon-css/css/flag-icon.min.css';
+import '../assets/css/gaxon-icon/style.css';
+import '../assets/css/perfect-scrollbar/css/perfect-scrollbar.css';
+import '../assets/css/lite-style-1.min.css';
+
+import '../assets/js/jquery/dist/jquery.min.js';
+import '../assets/js/moment/moment.js';
+import '../assets/js/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../assets/js/perfect-scrollbar/dist/perfect-scrollbar.min.js';
+import '../assets/js/masonry-layout/dist/masonry.pkgd.min.js';
+import '../assets/js/sweetalert2/dist/sweetalert2.js';
+import '../assets/js/chart.js/dist/Chart.min.js';
+import '../assets/js/script.js';
+import '../assets/js/custom/charts/dashboard-crypto.js';
+
+console.log('workng');
